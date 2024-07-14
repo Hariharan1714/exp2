4e26b551956cd244884994cc9cb9ea248adca3de
hariharan <hariharan10420@gmail.com>
2024-07-14 11:13:26 +0530

    adding pagination

--- public/ExpenseTracker/index.js
+++ public/ExpenseTracker/index.js
@@ -1,29 +1,87 @@
-function addNewExpense(e){
+let currentPage = 1;
+const itemsPerPage = 10;
+let expenses = [];
+
+function addNewExpense(e) {
     e.preventDefault();
 
     const expenseDetails = {
         expenseamount: e.target.expenseamount.value,
         description: e.target.description.value,
         category: e.target.category.value,
+    };
 
+    const token = localStorage.getItem('token');
+    axios.post('http://54.206.117.39:3000/expense/addexpense', expenseDetails, { headers: {"Authorization" : token} })
+        .then(response => {
+            expenses.push(response.data.expense);
+            displayExpenses();
+        })
+        .catch(err => showError(err));
+}
+
+function displayExpenses() {
+    const listOfExpenses = document.getElementById('listOfExpenses');
+    listOfExpenses.innerHTML = '';
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const currentExpenses = expenses.slice(start, end);
+
+    currentExpenses.forEach(expense => {
+        const expenseElem = document.createElement('li');
+        expenseElem.id = `expense-${expense.id}`;
+        expenseElem.innerHTML = `
+            ${expense.expenseamount} - ${expense.category} - ${expense.description}
+            <button onclick='deleteExpense(event, ${expense.id})'>Delete Expense</button>
+        `;
+        listOfExpenses.appendChild(expenseElem);
+    });
+
+    updatePaginationInfo();
+}
+
+function updatePaginationInfo() {
+    const pageInfo = document.getElementById('page-info');
+    const totalPages = Math.ceil(expenses.length / itemsPerPage);
+    pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
+}
+
+function nextPage() {
+    const totalPages = Math.ceil(expenses.length / itemsPerPage);
+    if (currentPage < totalPages) {
+        currentPage++;
+        displayExpenses();
     }
-    console.log(expenseDetails)
-    const token  = localStorage.getItem('token')
-    axios.post('http://54.206.117.39:3000/expense/addexpense',expenseDetails,  { headers: {"Authorization" : token} })
-        .then((response) => {
+}
 
-        addNewExpensetoUI(response.data.expense);
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        displayExpenses();
+    }
+}
 
-    }).catch(err => showError(err))
+function deleteExpense(e, expenseId) {
+    const token = localStorage.getItem('token');
+    axios.delete(`http://54.206.117.39:3000/expense/deleteexpense/${expenseId}`, { headers: {"Authorization" : token} })
+        .then(() => {
+            expenses = expenses.filter(expense => expense.id !== expenseId);
+            displayExpenses();
+        })
+        .catch(err => showError(err));
+}
 
+function showError(err) {
+    document.body.innerHTML += `<div style="color:red;">${err}</div>`;
 }
 
 function showPremiumuserMessage() {
-    document.getElementById('rzp-button1').style.visibility = "hidden"
-    document.getElementById('message').innerHTML = "You are a premium user "
+    document.getElementById('rzp-button1').style.visibility = "hidden";
+    document.getElementById('message').innerHTML = "You are a premium user";
 }
 
-function parseJwt (token) {
+function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
@@ -33,105 +91,64 @@ function parseJwt (token) {
     return JSON.parse(jsonPayload);
 }
 
-window.addEventListener('DOMContentLoaded', ()=> {
-    const token  = localStorage.getItem('token')
-    const decodeToken = parseJwt(token)
-    console.log(decodeToken)
-    const ispremiumuser = decodeToken.ispremiumuser
-    if(ispremiumuser){
-        showPremiumuserMessage()
-        showLeaderboard()
+window.addEventListener('DOMContentLoaded', () => {
+    const token = localStorage.getItem('token');
+    const decodeToken = parseJwt(token);
+    const ispremiumuser = decodeToken.ispremiumuser;
+    if (ispremiumuser) {
+        showPremiumuserMessage();
+        showLeaderboard();
     }
-    axios.get('http://54.206.117.39:3000/expense/getexpenses', { headers: {"Authorization" : token} })
-    .then(response => {
-            response.data.expenses.forEach(expense => {
 
-                addNewExpensetoUI(expense);
-            })
-    }).catch(err => {
-        showError(err)
-    })
+    axios.get('http://54.206.117.39:3000/expense/getexpenses', { headers: {"Authorization" : token} })
+        .then(response => {
+            expenses = response.data.expenses;
+            displayExpenses();
+        })
+        .catch(err => showError(err));
 });
 
-function addNewExpensetoUI(expense){
-    const parentElement = document.getElementById('listOfExpenses');
-    const expenseElemId = `expense-${expense.id}`;
-    parentElement.innerHTML += `
-        <li id=${expenseElemId}>
-            ${expense.expenseamount} - ${expense.category} - ${expense.description}
-            <button onclick='deleteExpense(event, ${expense.id})'>
-                Delete Expense
-            </button>
-        </li>`
-}
+function showLeaderboard() {
+    const inputElement = document.createElement("input");
+    inputElement.type = "button";
+    inputElement.value = 'Show Leaderboard';
+    inputElement.onclick = async () => {
+        const token = localStorage.getItem('token');
+        const userLeaderBoardArray = await axios.get('http://54.206.117.39:3000/premium/showLeaderBoard', { headers: {"Authorization" : token} });
 
-function deleteExpense(e, expenseid) {
-    const token = localStorage.getItem('token')
-    axios.delete(`http://54.206.117.39:3000/expense/deleteexpense/${expenseid}`,  { headers: {"Authorization" : token} }).then(() => {
-
-            removeExpensefromUI(expenseid);
-
-    }).catch((err => {
-        showError(err);
-    }))
-}
-
-function showError(err){
-    document.body.innerHTML += `<div style="color:red;"> ${err}</div>`
-}
-function showLeaderboard(){
-    const inputElement = document.createElement("input")
-    inputElement.type = "button"
-    inputElement.value = 'Show Leaderboard'
-    inputElement.onclick = async() => {
-        const token = localStorage.getItem('token')
-        const userLeaderBoardArray = await axios.get('http://54.206.117.39:3000/premium/showLeaderBoard', { headers: {"Authorization" : token} })
-        console.log(userLeaderBoardArray)
-
-        var leaderboardElem = document.getElementById('leaderboard')
-        leaderboardElem.innerHTML += '<h1> Leader Board </<h1>'
+        var leaderboardElem = document.getElementById('leaderboard');
+        leaderboardElem.innerHTML = '<h1> Leader Board </h1>';
         userLeaderBoardArray.data.forEach((userDetails) => {
-            leaderboardElem.innerHTML += `<li>Name - ${userDetails.name} Total Expense - ${userDetails.total_cost || 0} </li>`
-        })
-    }
+            leaderboardElem.innerHTML += `<li>Name - ${userDetails.name} Total Expense - ${userDetails.total_cost || 0}</li>`;
+        });
+    };
     document.getElementById("message").appendChild(inputElement);
-
-}
-
-function removeExpensefromUI(expenseid){
-    const expenseElemId = `expense-${expenseid}`;
-    document.getElementById(expenseElemId).remove();
 }
 
 document.getElementById('rzp-button1').onclick = async function (e) {
-    const token = localStorage.getItem('token')
-    const response  = await axios.get('http://54.206.117.39:3000/purchase/premiummembership', { headers: {"Authorization" : token} });
-    console.log(response);
-    var options =
-    {
-     "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
-     "order_id": response.data.order.id,// For one time payment
-     // This handler function will handle the success payment
-     "handler": async function (response) {
-        const res = await axios.post('http://54.206.117.39:3000/purchase/updatetransactionstatus',{
-             order_id: options.order_id,
-             payment_id: response.razorpay_payment_id,
-         }, { headers: {"Authorization" : token} })
-        
-        console.log(res)
-         alert('You are a Premium User Now')
-         document.getElementById('rzp-button1').style.visibility = "hidden"
-         document.getElementById('message').innerHTML = "You are a premium user "
-         localStorage.setItem('token', res.data.token)
-         showLeaderboard()
-     },
-  };
-  const rzp1 = new Razorpay(options);
-  rzp1.open();
-  e.preventDefault();
+    const token = localStorage.getItem('token');
+    const response = await axios.get('http://54.206.117.39:3000/purchase/premiummembership', { headers: {"Authorization" : token} });
+    var options = {
+        "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
+        "order_id": response.data.order.id, // For one time payment
+        "handler": async function (response) {
+            const res = await axios.post('http://54.206.117.39:3000/purchase/updatetransactionstatus', {
+                order_id: options.order_id,
+                payment_id: response.razorpay_payment_id,
+            }, { headers: {"Authorization" : token} });
+            
+            alert('You are a Premium User Now');
+            document.getElementById('rzp-button1').style.visibility = "hidden";
+            document.getElementById('message').innerHTML = "You are a premium user";
+            localStorage.setItem('token', res.data.token);
+            showLeaderboard();
+        },
+    };
+    const rzp1 = new Razorpay(options);
+    rzp1.open();
+    e.preventDefault();
 
-  rzp1.on('payment.failed', function (response){
-    console.log(response)
-    alert('Something went wrong')
- });
-}
+    rzp1.on('payment.failed', function (response) {
+        alert('Something went wrong');
+    });
+};
