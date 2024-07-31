87885f9bd77d2e02836b2d4e02302e307e63af62
hariharan <hariharan10420@gmail.com>
2024-07-31 14:01:02 +0530

    ip update

--- public/Signup/signup.js
+++ public/Signup/signup.js
@@ -10,7 +10,7 @@ async function signup(e) {
 
         }
         console.log(signupDetails)
-        const response  = await axios.post('http://3.24.215.182:3000/user/signup',signupDetails)
+        const response  = await axios.post('http://52.64.58.50:3000/user/signup',signupDetails)
             if(response.status === 201){
                 window.location.href = "../Login/login.html" // change the page on successful login
             } else {
